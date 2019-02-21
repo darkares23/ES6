347ce17985fb8388632d9311108fedcc7f89fdba
darkares23 <45240616+darkares23@users.noreply.github.com>
2019-02-21 09:08:35 -0500

    ES6- Primer commit

---
+++ es5.js
@@ -0,0 +1,17 @@
+let saludo = "hi, this is the new ES6";
+console.log(saludo);
+
+// Constant variables and sum.
+const x = 20;
+
+suma = function (n1, n2) {
+  return suma = n1 + n2;
+};
+console.log(suma(x, 10));
+
+//while
+var years = 0;
+while (years <= 5) {
+  console.log('ya llevo ' + years + ' años programando');
+  years++;
+}
